b1bbce44ff9b6e35554730f2e3757b82ce440f12
Zarcam <alejandrosatan.2896@gmail.com>
2023-11-17 01:00:07 +0100

    Added Adapter, GridLayout

--- app/src/main/java/com/example/juegoparejaspmdm/CartaAdapter.kt
+++ app/src/main/java/com/example/juegoparejaspmdm/CartaAdapter.kt
@@ -1,24 +1,31 @@
 package com.example.juegoparejaspmdm
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
-class CartaAdapter : RecyclerView.Adapter<CartaAdapter.CardView>() {
-    class CardView(view: View) : ViewHolder(view){
-
+class CartaAdapter(private val categoria: Categoria) : RecyclerView.Adapter<CartaAdapter.CardViewHolder>() {
+    class CardViewHolder(view: View) : ViewHolder(view){
+        val image: ImageView
+        init {
+            image = view.findViewById(R.id.cardImageView)
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardView {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card, parent, false)
+
+        return CardViewHolder(view)
+
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return categoria.getCartas().size
     }
 
-    override fun onBindViewHolder(holder: CardView, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
+        holder.image.setImageResource(this.categoria.getCartas()[position].getIdImagen())
     }
 }
--- app/src/main/java/com/example/juegoparejaspmdm/MainActivity.kt
+++ app/src/main/java/com/example/juegoparejaspmdm/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.juegoparejaspmdm
 
-import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
@@ -12,19 +10,29 @@ import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.navigation.NavigationView
+import com.example.juegoparejaspmdm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val mapCategorias: HashMap<String, Categoria> = HashMap()
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        val vistaJuego = findViewById<RecyclerView>(R.id.vistaJuego)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         setSupportActionBar(findViewById(R.id.barraHerramientas))
 
-        findViewById<Button>(R.id.restartButton).setOnClickListener{
+        binding.restartButton.setOnClickListener{
             Log.d("Boton", "Restart")
         }
+
+        initCategorias()
+
+        var adaptador = mapCategorias.get("Comida")?.let { CartaAdapter(it) }
+        val layoutManager = GridLayoutManager(this, 4)
+        binding.gridJuego.layoutManager = layoutManager
+        binding.gridJuego.adapter = adaptador
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -46,5 +54,32 @@ class MainActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    fun initCategorias(){
+        val categoriaComida = Categoria("Comida")
+        categoriaComida.addAll(arrayOf(
+            Carta(R.drawable.banana_blackbarry_blackberries_svgrepo_com),
+            Carta(R.drawable.cerejas_cherries_cherry_svgrepo_com),
+            Carta(R.drawable.cupcake_svgrepo_com),
+            Carta(R.drawable.donut_svgrepo_com),
+            Carta(R.drawable.fruit_melancia_watermelon_svgrepo_com),
+            Carta(R.drawable.hamburger_svgrepo_com),
+            Carta(R.drawable.orange_svgrepo_com),
+            Carta(R.drawable.pizza_svgrepo_com)))
+
+        val categoriaAnimales = Categoria("Animales")
+        categoriaAnimales.addAll(arrayOf(
+            Carta(R.drawable.bear_svgrepo_com),
+            Carta(R.drawable.bird_svgrepo_com),
+            Carta(R.drawable.camel_svgrepo_com),
+            Carta(R.drawable.cat_with_wry_smile_svgrepo_com),
+            Carta(R.drawable.crocodile_svgrepo_com),
+            Carta(R.drawable.dog_face_svgrepo_com),
+            Carta(R.drawable.dolphin_svgrepo_com),
+            Carta(R.drawable.mouse_svgrepo_com)))
+
+        mapCategorias.put(categoriaComida.getNombre(), categoriaComida)
+        mapCategorias.put(categoriaAnimales.getNombre(), categoriaAnimales)
     }
 }
